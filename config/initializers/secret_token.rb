fe612dd2070ee07b38ce025a04089a52d0e53a1e
Paul Wittmann <paul@railslove.com>
2012-09-14 22:09:35 +0200

    bundle update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Irmingard::Application.config.secret_token = '1a328a97f5fc871e16974c18dad8afd25de33472996846dae9e26f7ddfe194dd49967577a05e1c84b9019f22ca39635c9a02e0d7ba3cbc4d1eff474a772a6db1'
+Irmingard::Application.config.secret_token = '7f2adadf6eea85a633ec1487d288776b19cda45a5762c7004a0c891798e9170e96de70bb6b87f96102ac2db7a66e963808c243044aef53e6df40cad452840f95'
